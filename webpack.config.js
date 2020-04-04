8664df6f2ca31e59c563de11519a5f617eb77313
Josh Brown <josh@joossh.com>
2020-04-04 11:29:27 +0100

    🔧 Update webpack config to support production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,58 @@ const path = require('path')
 const ExtensionReloader = require('webpack-extension-reloader')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-  entry: {
-    content: './src/content.js',
-    background: './src/background.js',
-  },
-  mode: 'development',
-  watch: true,
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              [
-                'babel-plugin-styled-components',
-                { fileName: false, displayName: true },
+module.exports = env => {
+  if (!env || !env.NODE_ENV)
+    throw new Error('NODE_ENV must be set using --env.NODE_ENV=production')
+
+  if (env.NODE_ENV !== 'production' && env.NODE_ENV !== 'development')
+    throw new Error(
+      'Webpack configuration only supports production or development as NODE_ENV',
+    )
+
+  const developmentPlugins =
+    env.NODE_ENV === 'development'
+      ? [
+          new ExtensionReloader({
+            manifest: path.resolve(__dirname, 'src', 'manifest.json'),
+          }),
+        ]
+      : []
+
+  return {
+    entry: {
+      content: './src/content.js',
+      background: './src/background.js',
+    },
+    mode: env.NODE_ENV,
+    watch: env.NODE_ENV === 'development',
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                [
+                  'babel-plugin-styled-components',
+                  { fileName: false, displayName: true },
+                ],
               ],
-            ],
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
           },
         },
-      },
+      ],
+    },
+    plugins: [
+      ...developmentPlugins,
+      new CopyWebpackPlugin([{ from: './src/manifest.json' }]),
     ],
-  },
-  plugins: [
-    new ExtensionReloader({
-      manifest: path.resolve(__dirname, 'src', 'manifest.json'),
-    }),
-    new CopyWebpackPlugin([{ from: './src/manifest.json' }]),
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+    },
+  }
 }
